60c3e32610996f71307650a0465067230957299f
blaiseludvig <blaiseludvig@gmail.com>
2022-09-28 11:21:11 +0200

    complete the task

---
+++ zene.js
@@ -0,0 +1,45 @@
+const title = document.querySelector("#track-title");
+const length = document.querySelector("#track-length");
+const btn_add = document.querySelector("#add");
+const output = document.querySelector("#out");
+
+class Track {
+  title = "";
+  length = 0;
+
+  static playlist = [];
+
+  constructor(title, length) {
+    this.title = title;
+    this.length = parseInt(length);
+  }
+
+  static total_length() {
+    let total = 0;
+
+    this.playlist.forEach((track) => {
+      total += track.length;
+    });
+
+    return total;
+
+  }
+
+}
+
+function init() {
+  btn_add.addEventListener("click", () => {
+    if (title.value == "" || length.value == "") {
+      return;
+    }
+
+    Track.playlist.push(new Track(title.value, length.value));
+    title.value = "";
+    length.value = "";
+
+    output.textContent = `${Track.total_length()} perc`;
+  });
+  
+}
+
+init();
